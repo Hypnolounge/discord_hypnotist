7adf0e9d9446dbb89e5d78bd5794c53cde7fa598
PupNicky <sailgamer03@gmail.com>
2024-07-16 21:11:24 +0200

    Update repo to nuxt

---
+++ frontend/components/LoginButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-btn icon="discord">
+        Button
+    </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // type inference enabled
+  data() {
+    return {
+      link: process.env.DISCORD_REDIRECT_URI,
+    };
+  },
+});
+
+</script>
